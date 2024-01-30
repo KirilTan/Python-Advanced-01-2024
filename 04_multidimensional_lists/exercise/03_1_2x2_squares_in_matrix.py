0cc679c216ecbbf516b628e140ab72892a8c18a9
KirilTan <kiriltanev@proton.me>
2024-01-30 14:00:02 +0200

    04_exercise 03_1 updated

--- 04_multidimensional_lists/exercise/03_1_2x2_squares_in_matrix.py
+++ 04_multidimensional_lists/exercise/03_1_2x2_squares_in_matrix.py
@@ -1,17 +1,23 @@
 rows, columns = [int(num) for num in input().split()]
 string_matrix = [[el for el in input().split()] for _ in range(rows)]
 
+# Example:
+# 3 4
+# A B B D
+# E B B B
+# I J B B
+
 sub_matrix_with_same_values = 0
 
 for row_index in range(rows - 1):
     for column_index in range(columns - 1):
-        sub_matrix = [
-            string_matrix[row_index][column_index],
-            string_matrix[row_index][column_index + 1],
-            string_matrix[row_index + 1][column_index],
-            string_matrix[row_index + 1][column_index + 1]
-        ]
-        first_element = sub_matrix[0][0]
+
+        sub_matrix = [                                                                                      # [
+            string_matrix[row_index][column_index], string_matrix[row_index][column_index + 1],             # [A], [B],
+            string_matrix[row_index + 1][column_index], string_matrix[row_index + 1][column_index + 1]      # [E], [B]
+        ]                                                                                                   # ]
+
+        first_element = sub_matrix[0][0]                                                                    # [A]
 
         is_valid = True
         for row in sub_matrix:
