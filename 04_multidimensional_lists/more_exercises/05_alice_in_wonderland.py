546dfba2200d366aa64ddef495e35328bdadcaa7
KirilTan <kiriltanev@proton.me>
2024-02-01 17:54:48 +0200

    04_multidimensional_lists -> more_exercises -> 05_alice_in_wonderland.py
    solved

---
+++ 04_multidimensional_lists/more_exercises/05_alice_in_wonderland.py
@@ -0,0 +1,47 @@
+matrix_size = int(input())
+
+matrix = []
+alice_pos = []
+
+tea_bags = 0
+
+directions = {
+    'up': (-1, 0),
+    'down': (1, 0),
+    'left': (0, -1),
+    'right': (0, 1)
+}
+
+for row in range(matrix_size):
+    matrix.append(input().split())
+
+    if 'A' in matrix[row]:
+        alice_pos = [row, matrix[row].index('A')]
+        matrix[row][alice_pos[1]] = '*'
+
+while tea_bags < 10:
+    direction = input()
+
+    row = alice_pos[0] + directions[direction][0]
+    col = alice_pos[1] + directions[direction][1]
+
+    if not (0 <= row < matrix_size and 0 <= col < matrix_size):
+        break
+
+    alice_pos = [row, col]
+    position = matrix[row][col]
+    matrix[row][col] = '*'
+
+    if position == 'R':
+        break
+
+    if position.isdigit():
+        tea_bags += int(position)
+
+if tea_bags < 10:
+    print("Alice didn't make it to the tea party.")
+else:
+    print("She did it! She went to the party.")
+
+for row in matrix:
+    print(' '.join(row))
